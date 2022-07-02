b50df54102a47e4cef3948607ed4b5c7df2b8eb4
saravanakumar <saravanakumar@nmsworks.co.in>
2022-07-02 11:01:59 +0530

    Stateful_Widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,64 +1,149 @@
-
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: Home(),
+  home: NinjaCard(),
 ));
 
-class Home extends StatelessWidget {
-  const Home({Key? key}) : super(key: key);
+
+class NinjaCard extends StatefulWidget {
+  @override
+  State<NinjaCard> createState() => _NinjaCardState();
+}
+
+class _NinjaCardState extends State<NinjaCard> {
+
+  int level = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[900],
       appBar: AppBar(
-          title: const Text('my first app'),
-          centerTitle: true,
-          backgroundColor: Colors.red[600]
+        title: Text('Ninja ID Card'),
+        centerTitle: true,
+        backgroundColor: Colors.grey[850],
+        elevation: 0.0,
       ),
-      body: Center(
-        child: IconButton(
-          onPressed: () {
-            print('you clicked me');
-          },
-          icon: Icon(Icons.alternate_email),
-          color: Colors.amber,
-        ),
-      ),
+
+
       floatingActionButton: FloatingActionButton(
-        backgroundColor: Colors.red[600],
-        onPressed: () {  },
-        child: const Text('click'),
+        onPressed: () {
+          setState(() {
+            level+=1;
+          });
+        },
+        mini: true,
+        tooltip: "Adder",
+
+        child: Icon(Icons.add,color:Colors.blue.shade900,size: 20.0,),
+
+      ),
+
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Center(
+              child: CircleAvatar(
+                radius: 40.0,
+                backgroundImage: AssetImage('assets/flutter.png'),
+              ),
+            ),
+            Divider(
+              color: Colors.grey[800],
+              height: 30.0,
+            ),
+            Text(
+              'NAME',
+              style: TextStyle(
+                color: Colors.grey,
+                letterSpacing: 2.0,
+              ),
+            ),
+            SizedBox(height: 10.0),
+            Text(
+              'Chun-Li',
+              style: TextStyle(
+                color: Colors.amberAccent[200],
+                fontWeight: FontWeight.bold,
+                fontSize: 18.0,
+                letterSpacing: 2.0,
+              ),
+            ),
+            SizedBox(height: 30.0),
+            Text(
+              'HOMETOWN',
+              style: TextStyle(
+                color: Colors.grey,
+                letterSpacing: 2.0,
+              ),
+            ),
+            SizedBox(height: 10.0),
+            Text(
+              'Beijing, China',
+              style: TextStyle(
+                color: Colors.amberAccent[200],
+                fontWeight: FontWeight.bold,
+                fontSize: 18.0,
+                letterSpacing: 2.0,
+              ),
+            ),
+            SizedBox(height: 30.0),
+            Text(
+              'CURRENT NINJA LEVEL',
+              style: TextStyle(
+                color: Colors.grey,
+                letterSpacing: 2.0,
+              ),
+            ),
+            SizedBox(height: 10.0),
+            Text(
+              '$level',
+              style: TextStyle(
+                color: Colors.amberAccent[200],
+                fontWeight: FontWeight.bold,
+                fontSize: 18.0,
+                letterSpacing: 2.0,
+              ),
+            ),
+            SizedBox(height: 30.0),
+            Row(
+              children: <Widget>[
+                Icon(
+                  Icons.email,
+                  color: Colors.grey[400],
+                ),
+                SizedBox(width: 10.0),
+                Text(
+                  'chun.li@thenetninja.co.uk',
+                  style: TextStyle(
+                    color: Colors.grey[400],
+                    fontSize: 18.0,
+                    letterSpacing: 1.0,
+                  ),
+                )
+
+              ],
+            ),
+            SizedBox(
+              height: 33.0,
+            ),
+            Container(
+              child: FloatingActionButton(
+                onPressed: () {
+                  setState(() {
+                    level =0;
+                  });
+                },
+                mini: true,
+                tooltip: "Refresh",
+                backgroundColor: Colors.blue.shade800,
+                child: const Icon(Icons.refresh),
+              ),
+            ),  ],
+        ),
       ),
     );
   }
 }
-
-// snippets for icons and buttons
-
-//  Icon(
-//    Icons.airport_shuttle,
-//    color: Colors.lightBlue,
-//    size: 50.0
-//  ),
-
-//  RaisedButton(
-//    onPressed: () {
-//      print('you clicked me');
-//    },
-//    child: Text('click me'),
-//    color: Colors.lightBlue,
-//  ),
-
-//  FlatButton(
-//    onPressed: () {},
-//    child: Text('click me again'),
-//    color: Colors.amber
-//  ),
-
-//  RaisedButton.icon(
-//    onPressed: () {},
-//    icon: Icon(Icons.mail),
-//    label: Text('mail me'),
-//    color: Colors.amber,
-//  ),
