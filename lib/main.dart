81b655fc39542cd18bfd3b40359d54fd722dd46a
saravanakumar <saravanakumar@nmsworks.co.in>
2022-07-02 11:56:15 +0530

    List of data

--- lib/main.dart
+++ lib/main.dart
@@ -1,149 +1,39 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
 
 void main() => runApp(MaterialApp(
-  home: NinjaCard(),
+  home: Quotes(),
 ));
 
-
-class NinjaCard extends StatefulWidget {
+class Quotes extends StatefulWidget {
   @override
-  State<NinjaCard> createState() => _NinjaCardState();
+  _QuotesState createState() => _QuotesState();
 }
 
-class _NinjaCardState extends State<NinjaCard> {
+class _QuotesState extends State<Quotes> {
 
-  int level = 0;
+  List<String> quotes = [
+    'Be yourself; everyone else is already taken',
+    'I have nothing to declare except my genius',
+    'The truth is rarely pure and never simple'
+  ];
+
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey[900],
-      appBar: AppBar(
-        title: Text('Ninja ID Card'),
-        centerTitle: true,
-        backgroundColor: Colors.grey[850],
-        elevation: 0.0,
-      ),
-
-
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          setState(() {
-            level+=1;
-          });
-        },
-        mini: true,
-        tooltip: "Adder",
-
-        child: Icon(Icons.add,color:Colors.blue.shade900,size: 20.0,),
-
-      ),
-
-      body: Padding(
-        padding: const EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0),
+      backgroundColor: Colors.cyan.shade50,
+      appBar: AppBar(title: const Text("Today Quotes"),centerTitle: true,backgroundColor: Colors.grey.shade800,elevation: 30),
+      body: Center(
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Center(
-              child: CircleAvatar(
-                radius: 40.0,
-                backgroundImage: AssetImage('assets/flutter.png'),
-              ),
-            ),
-            Divider(
-              color: Colors.grey[800],
-              height: 30.0,
-            ),
-            Text(
-              'NAME',
-              style: TextStyle(
-                color: Colors.grey,
-                letterSpacing: 2.0,
-              ),
-            ),
-            SizedBox(height: 10.0),
-            Text(
-              'Chun-Li',
-              style: TextStyle(
-                color: Colors.amberAccent[200],
-                fontWeight: FontWeight.bold,
-                fontSize: 18.0,
-                letterSpacing: 2.0,
-              ),
-            ),
-            SizedBox(height: 30.0),
-            Text(
-              'HOMETOWN',
-              style: TextStyle(
-                color: Colors.grey,
-                letterSpacing: 2.0,
-              ),
-            ),
-            SizedBox(height: 10.0),
-            Text(
-              'Beijing, China',
-              style: TextStyle(
-                color: Colors.amberAccent[200],
-                fontWeight: FontWeight.bold,
-                fontSize: 18.0,
-                letterSpacing: 2.0,
-              ),
-            ),
-            SizedBox(height: 30.0),
-            Text(
-              'CURRENT NINJA LEVEL',
-              style: TextStyle(
-                color: Colors.grey,
-                letterSpacing: 2.0,
-              ),
-            ),
-            SizedBox(height: 10.0),
-            Text(
-              '$level',
-              style: TextStyle(
-                color: Colors.amberAccent[200],
-                fontWeight: FontWeight.bold,
-                fontSize: 18.0,
-                letterSpacing: 2.0,
-              ),
-            ),
-            SizedBox(height: 30.0),
-            Row(
-              children: <Widget>[
-                Icon(
-                  Icons.email,
-                  color: Colors.grey[400],
-                ),
-                SizedBox(width: 10.0),
-                Text(
-                  'chun.li@thenetninja.co.uk',
-                  style: TextStyle(
-                    color: Colors.grey[400],
-                    fontSize: 18.0,
-                    letterSpacing: 1.0,
-                  ),
-                )
-
-              ],
-            ),
-            SizedBox(
-              height: 33.0,
-            ),
-            Container(
-              child: FloatingActionButton(
-                onPressed: () {
-                  setState(() {
-                    level =0;
-                  });
-                },
-                mini: true,
-                tooltip: "Refresh",
-                backgroundColor: Colors.blue.shade800,
-                child: const Icon(Icons.refresh),
-              ),
-            ),  ],
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: quotes.map((quote) => Text(quote)).toList()
         ),
       ),
     );
   }
 }
+
+
